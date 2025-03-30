b3dfe889967a3f8b156b1f53a5d84b687caaaec7
Alsiddig abdalwahed <126944216+Alsiddigabdo@users.noreply.github.com>
2025-03-30 20:37:00 +0200

    Update smartTranslation.js

--- routes/smartTranslation.js
+++ routes/smartTranslation.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const SmartTranslationController = require('../controllers/SmartTranslationController');
+const { authenticateToken } = require('../controllers/AuthController');
 
-router.get('/', SmartTranslationController.renderTranslationPage);
-router.post('/translate', SmartTranslationController.translateText);
-router.post('/improve', SmartTranslationController.improveTranslation);
+router.get('/', SmartTranslationController.renderTranslationPage); // صفحة عامة
+router.post('/translate', authenticateToken, SmartTranslationController.translateText); // محمية
+router.post('/improve', authenticateToken, SmartTranslationController.improveTranslation); // محمية
 
 module.exports = router;
