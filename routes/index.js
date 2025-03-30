58fe458095f6983cfd84cf1c1889768f244d980c
Alsiddig abdalwahed <126944216+Alsiddigabdo@users.noreply.github.com>
2025-03-30 20:21:19 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { authenticateToken } = require('../controllers/AuthController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  const user = req.session.user || null; // تمرير بيانات المستخدم إذا كان مسجل الدخول
+/* GET home page */
+router.get('/', (req, res, next) => {
+  const user = req.user || null; // استخدام req.user من JWT بدلاً من req.session.user
   res.render('index', { title: 'الصفحة الرئيسية', user });
 });
 
 /* GET About page */
-router.get('/about', function(req, res, next) {
-  const user = req.session.user || null;
+router.get('/about', (req, res, next) => {
+  const user = req.user || null;
   res.render('About', { title: 'عن التطبيق', user });
 });
 
 /* GET Privacy Policy page */
-router.get('/privacy-policy', function(req, res, next) {
-  const user = req.session.user || null;
+router.get('/privacy-policy', (req, res, next) => {
+  const user = req.user || null;
   res.render('PrivacyPolicy', { title: 'سياسة الخصوصية', user });
 });
 
