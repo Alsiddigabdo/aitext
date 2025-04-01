32ea784eab49720303ede49fd7ae79db6ac62550
Alsiddig abdalwahed <126944216+Alsiddigabdo@users.noreply.github.com>
2025-04-01 11:35:44 +0200

    Update FeedbackController.js

--- controllers/FeedbackController.js
+++ controllers/FeedbackController.js
@@ -1,5 +1,5 @@
 const FeedbackModel = require('../models/FeedbackModel');
-const authenticateToken = require('./authenticateToken');
+
 
 class FeedbackController {
   static async renderFeedbackPage(req, res) {
